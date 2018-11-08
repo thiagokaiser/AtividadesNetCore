07c4854cf9c649d34a6ce555de6c655381bab3f6
Thiago Kaiser <thiago.kaiser@hotmail.com>
2018-11-08 14:49:35 -0200

    script sql

---
+++ DB/DropAndCreate dbo.Atividade.sql
@@ -0,0 +1,24 @@
+﻿USE [AtividadesDB]
+GO
+
+/****** Object: Table [dbo].[Atividade] Script Date: 08/11/2018 14:48:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [dbo].[Atividade];
+
+
+GO
+CREATE TABLE [dbo].[Atividade] (
+    [Id]          INT        IDENTITY (1, 1) NOT NULL,
+    [Descricao]   NCHAR (40) NULL,
+    [Responsavel] NCHAR (40) NULL,
+    [Setor]       NCHAR (40) NULL,
+    [CategoriaId] INT        NULL,
+    [Data]        DATE       NULL
+);
+
+
---
+++ DB/DropAndCreate dbo.Categoria.sql
@@ -0,0 +1,21 @@
+﻿USE [AtividadesDB]
+GO
+
+/****** Object: Table [dbo].[Categoria] Script Date: 08/11/2018 14:49:14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE [dbo].[Categoria];
+
+
+GO
+CREATE TABLE [dbo].[Categoria] (
+    [Id]        INT        IDENTITY (1, 1) NOT NULL,
+    [Descricao] NCHAR (20) NULL,
+    [Cor]       NCHAR (10) NULL
+);
+
+
